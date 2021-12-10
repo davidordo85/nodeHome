b709db8fe3295fe4b5e1a3c4d6c8f625152a76b6
David Ordoñez <davidordonezescacena@hotmail.es>
2021-12-10 21:04:04 +0100

    extension views and change title name

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+
+app.locals.title = 'NODEHOME';
 
 app.use(logger('dev'));
 app.use(express.json());
